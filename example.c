f58846f2ec6f0f0a5e4b2cc1ce336398807817e9
mwsoft69 <wodachm@gmail.com>
2019-11-20 11:20:56 -0600

    Remove unessary comments

--- example.c
+++ example.c
@@ -1,23 +1,13 @@
-#include "mtwkiss.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "mtwkiss.h"
 
 int main()
 {
 	
-	/*These values could be any unsigned int or 0 and then set with initRandSeed() */
-//	unsigned int x=0,y=0,z=0,c=0;
-
 	struct state *s;
 	s = malloc(sizeof(struct state));
 
-
-
-
-
-
-		
-	/*This example uses initRandSeed()*/
 	unsigned int randNum = 0;
 
 	printf("random number is %d\n",randNum);

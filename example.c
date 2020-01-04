241fed75188a64ede4104e0b1064841ef9e5fb7b
mitch <mitch@boxey>
2020-01-04 06:11:05 -0600

    Update example.c

--- example.c
+++ example.c
@@ -7,7 +7,13 @@ int main()
 	
 	struct state *s;
 	s = malloc(sizeof(struct state));
-
+	
+	/*set seeds using devRand(). You can also define your own seeds also.*/
+	s->x = devRand();
+	s->y = devRand();
+	s->z = devRand();
+	s->c = devRand();
+		
 	unsigned int randNum = Kiss(s);
 
 	printf("random number is %d\n",randNum);
